7d9abaf53f5753c5b3ee0b1f677dfb4d4cd3b6b9
diegorosas19 <diegorosas19@gmail.com>
2021-06-02 15:07:00 -0300

    Aula 03

--- Aulas/Aula 03/EX03.py
+++ Aulas/Aula 03/EX03.py
@@ -1,6 +1,6 @@
 #Uso da função PRINT, INPUT e INT
 
-#Transformando a entrada antes de gravar na variável 
+#Transformando a o valor da entrada antes de gravar na variável 
 
 a = int(input("Digite a idade da pessoa A: "))
 b = int(input("Digite a idade da pessoa B: "))
