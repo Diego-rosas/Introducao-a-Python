9fb2c8a79f3f90a4768112afa34b87ff55da1982
diegorosas19 <diegorosas19@gmail.com>
2021-06-02 15:35:30 -0300

    Aula 04 - Ex01

---
+++ Aulas/Aula 04/Ex01.py
@@ -0,0 +1,6 @@
+idade1 = int(input('Digite a primeira idade: '))
+idade2 = int(input('Digite a segunda idade: '))
+
+diferencaIdades = idade1 - idade2
+
+print ('A diferença entre as idades é: ', diferencaIdades)
