8f78ac9d03a46c94acfde1ab4b4e3d6d003efc6d
diegorosas19 <diegorosas19@gmail.com>
2021-06-02 21:14:08 -0300

    Aula 04 - Ex04

---
+++ Aulas/Aula 04/EX04.py
@@ -0,0 +1,9 @@
+texto = 'Eu quero programar!'
+
+print(texto[0],texto[3],texto[9], texto[18]) #imprimindo as primeiras letras e a exclamação da forma normal
+
+print(texto[0:1], texto[3:4], texto[9:10], texto[18:19]) #imprimindo as primeiras letras de todas as palavras por fatiamento e a exclamação 
+
+print(texto[9:19]) #imprimindo a palavra programar e a exclamação
+
+print(texto[-2:-1])
