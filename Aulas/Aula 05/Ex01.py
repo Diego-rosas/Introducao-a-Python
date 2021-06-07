879c6fac98ad7d41ccb2bdec10003afadd1cf830
Diego-rosas <diegorosas19@gmail.com>
2021-06-07 16:27:48 -0300

    Aula 05 - Execicio 01

---
+++ Aulas/Aula 05/Ex01.py
@@ -0,0 +1,10 @@
+#Estruturas Condicionais
+#Uso do comando IF
+
+idade1 = int(input("Digite a primeira idade:"))
+idade2 = int(input("Digite a segunda idade:"))
+
+if idade1 > idade2:
+    print("A idade", idade1, "é a maior")
+else :
+    print("A idade", idade2, "é a maior")
