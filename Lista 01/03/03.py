acfa36014948d88e2a678146a0874b08603eaf83
Diego-rosas <diegorosas19@gmail.com>
2021-06-09 19:52:48 -0300

    Questao 03

---
+++ Lista 01/03/03.py
@@ -0,0 +1,18 @@
+#Questao 03
+#por: Diego Rosas
+
+print('-------------------------------------------------------') #gambiarra para pular uma linha
+inteiro1 = int(input("Digite o primeiro numero inteiro: "))
+inteiro2 = int(input("Digite o segundo numero inteiro: "))
+reais = float(input("Digite um numero real: "))
+
+letraA = (inteiro1 * 2) * (inteiro2 // 2)
+letraB = (inteiro1 * 3) + reais
+letraC = (reais ** 3)
+
+print('')
+print('Resultado da letra a): ', letraA)
+print('Resultado da letra b): ', letraB)
+print('Resultado da letra c): ', letraC)
+
+print('-------------------------------------------------------')
