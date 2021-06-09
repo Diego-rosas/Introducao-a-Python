9a51ade3c6cbfd006881a92f17895c841823b550
Diego-rosas <diegorosas19@gmail.com>
2021-06-09 19:52:34 -0300

    Questao 02

---
+++ Lista 01/02/02.py
@@ -0,0 +1,13 @@
+#Questao 02
+#por: Diego Rosas
+
+print('--------------------------------------------------') #gambiarra para pular uma linha
+salario = int(input('Digite o valor do seu salario[R$]: ')) #Variavel que vai receber o valor do salario digitado pelo usuario
+
+print('')                                     #gambiarra para pular uma linha
+if salario > 1200:                            #Estrutura condicional que vai decidir qual mesnsagem dever ser impressa de acordo com a condicao passada 
+    print('  Você deverá pagar imposto!')     #imprime a mensagem resultado
+else:                                         #Estrutura condicional que vai decidir qual mesnsagem dever ser impressa de acordo com a condicao passada
+    print('  Você não deverá pagar imposto!') #imprime a mensagem resultado 
+
+print('--------------------------------------------------') #gambiarra para pular uma linha
