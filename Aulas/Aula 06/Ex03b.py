69107c733c61e3c7ec30e43555618d212db61a20
Diego-rosas <diegorosas19@gmail.com>
2021-07-01 09:04:52 -0300

    Aula 06 - Ex3b

---
+++ Aulas/Aula 06/Ex03b.py
@@ -0,0 +1,9 @@
+valor = int(input('Digite até que numero par vc que contar: '))
+
+i = 0 
+while i <= valor:
+    print(i)
+    i += 2
+
+#Esse programa imprime os somente os valores para a partir de 0 até o valor informado pelo usuario. 
+#Algoritmo contruido somente com o uso de uma estrutura de repeticao.
