94d9370971b050bedcd2cbf573d2278d3b0f7fe4
Diego-rosas <diegorosas19@gmail.com>
2021-07-01 09:04:34 -0300

    Aula 06 - Ex3a

---
+++ Aulas/Aula 06/Ex03a.py
@@ -0,0 +1,11 @@
+valor = int(input('Digite até que numero par vc que contar: '))
+
+i = 0
+
+while i <= valor:
+    if i % 2 == 0:
+        print(i)
+    i += 1    
+
+#Esse programa imprime os somente os valores para a partir de 0 até o valor informado pelo usuario. 
+# Algoritmo contruido com o uso de uma estrutura de repeticao aninhada com uma condicional.
