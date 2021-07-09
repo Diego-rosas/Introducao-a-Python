c425a27a7ebd45f8fc9bd1291a8cb7c54c6b69d6
Diego-rosas <diegorosas19@gmail.com>
2021-07-09 03:31:15 -0300

    Aula 12 - Ex1

---
+++ Aulas/Aula 12/Ex01.py
@@ -0,0 +1,7 @@
+import emojis
+
+print('\n',emojis.encode('This is a message with emojis :smile: :snake:'),'\n')
+#This is a message with emojis 😄 🐍')
+print(emojis.get('Prefix 😄 🐍 😄 🐍 Sufix'))
+
+print(emojis.encode(':snake:'))
