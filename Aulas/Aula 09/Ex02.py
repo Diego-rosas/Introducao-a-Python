52847f8f46ea3001991224b1ff3b5e0d53a4ed5c
Diego-rosas <diegorosas19@gmail.com>
2021-07-09 03:27:55 -0300

    Aula 09 - Ex2

---
+++ Aulas/Aula 09/Ex02.py
@@ -0,0 +1,18 @@
+
+T = ('A', 'A', 'C', 'F',  'X', 'E', 'P', 'E', 'Z')
+
+print(len(T))
+print('')
+
+print(T.count('A'))
+print('')
+
+print(sorted(T))
+print('')
+
+print(T.index('X'))
+print('')
+
+print(T[-1])
+print('')
+
