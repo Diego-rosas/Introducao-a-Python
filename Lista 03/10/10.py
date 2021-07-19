4b6810e1282229a45843b7fb18284eee7a9940d4
Diego-rosas <diegorosas19@gmail.com>
2021-07-19 00:14:12 -0300

    Inseri os comentarios

--- Lista 03/10/10.py
+++ Lista 03/10/10.py
@@ -7,38 +7,33 @@ import os
 import time
 import random
 
-
 #Criando duas listas com valores de  0 - 9
-list1 = list(range(0,10))
-list2 = list(range(0,10))
-embaralha_list1 = list1[0:len(list1)]
-embaralha_list2 = list2[0:len(list2)]
-sorteia_list1 = []
-sorteia_list2 = []
-dezenas1 = []
-dezenas2 = []
+list1 = list(range(0,10))                       #cria a lista 1 com numeros de 0-9
+list2 = list(range(0,10))                       #cria a lista 1 com numeros de 0-9
+embaralha_list1 = list1[0:len(list1)]           #copia a lista 1 para a lista embaralha
+embaralha_list2 = list2[0:len(list2)]           #copia a lista 1 para a lista embaralha
 
 #Embaralha as listas 
-random.shuffle(embaralha_list1)
-random.shuffle(embaralha_list2)
+random.shuffle(embaralha_list1)                 #embaralha a lista 1   
+random.shuffle(embaralha_list2)                 #embaralha a lista 2
 
 #sorteia um numero 5x das listas embaralhadas 1 e 2 
-while True:
+while True:                                         #loop principal
     print('='*45)                                   #começo do programa
-    print('     SOTEIO DA MEGA SENA DA VIRADA!')
+    print('     SOTEIO DA MEGA SENA DA VIRADA!')    #titulo
     print('='*45)
-    for i in range(5):
-        x = random.choice(embaralha_list1)
-        y = random.choice(embaralha_list2)
-        if x == y == 0:
-            x = random.choice(embaralha_list1)
+    for i in range(5):                            #loop para sortear os numeros das dezenas 
+        x = random.choice(embaralha_list1)        #A variavel X vai receber o primeiro numero da dezena
+        y = random.choice(embaralha_list2)        #A variavel y vai receber o segundo numero da dezena 
+        if x == y == 0:                            #Analisa se tera uma dezena '00'; 
+            x = random.choice(embaralha_list1)      #Se for sorteado um '00' repete o sorteio
             y = random.choice(embaralha_list2)
-        else:
-            print('{}ª Dezena sorteada: {}{}'.format(i + 1,x,y))
-        time.sleep(.5)
-    sai = input('\nDeseja sortear de novo?[S/N]: ').upper()
-    if sai == 'N':
-        print('\n  FIM DO SORTEIO!')
-        break
+        else:                                       #senao for sorteado uma dezena 00
+            print('{}ª Dezena sorteada: {}{}'.format(i + 1,x,y)) #segue para imprimir as dezenas sorteadas
+        time.sleep(.5)                                          #pausa de meio segundo entre as impressoes
+    sai = input('\nDeseja sortear de novo?[S/N]: ').upper()  #pergunta se quer sortear de novo
+    if sai == 'N':                                   #Se a resposta for sim limpa a tela e comeca de novo
+        print('\n  FIM DO SORTEIO!')                 #Sea resposta for nao, imprime a msg   
+        break                                        #encerra o programa 
         
-    os.system('cls')    
+    os.system('cls')                                 #limpa a tela para fazer outro sorteio 
