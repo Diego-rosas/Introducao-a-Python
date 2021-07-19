123869e238edfa8528f15c60637adbb965d3208d
Diego-rosas <diegorosas19@gmail.com>
2021-07-19 01:00:13 -0300

    update Questao 01

--- Lista 03/01/enviaEmail.py
+++ Lista 03/01/enviaEmail.py
@@ -8,7 +8,7 @@ def enviaemail(corpo, recebeEmail):
     host = 'smtp.gmail.com'
     port = '587'
     login = 'enviae7@gmail.com'
-    senha = 'seupassword'
+    senha = '#enviaEmail7' 
   
     server = smtplib.SMTP(host,port)
     server.ehlo()
