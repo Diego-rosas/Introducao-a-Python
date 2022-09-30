4758066c4ca1b53448c4b196238fc4aea0d1578b
Diego Roberto Gomes Rosas <diego.rosas@ifood.com.br>
2022-09-30 15:44:06 -0300

    excluindo arquivos

--- Programa de RPA_05.07.2021/teste.py
+++
@@ -1,9 +0,0 @@
-import time
-
-meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outrubro', 'novembro', 'dezembro']
-
-indiceMes = int(time.strftime('%m')) 
-
-mesAtual = meses[indiceMes - 1] 
-
-print(mesAtual)
